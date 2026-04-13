196e400f1963a1ac286dae49bfeadc9f81c521c5
byfetsey77-sudo <byfetsey77@gmail.com>
2026-04-13 23:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from discord.ui import Button, View
 import os
 from wakeonlan import send_magic_packet
 
-# --- AYARLAR ---
-# Render'da Environment Variables kısmına DISCORD_TOKEN eklemeyi unutma knk!
+# Render Environment Variables'dan çekilir
 TOKEN = os.getenv("DISCORD_TOKEN")
 MAC_ADRESI = '7C:10:C9:4A:2B:65' 
 DIS_IP = '78.182.3.1'
@@ -15,7 +14,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# --- PANEL (SADECE UYANDIRMA) ---
 class ControlPanel(View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -24,17 +22,17 @@ class ControlPanel(View):
     async def open_pc(self, interaction: discord.Interaction, button: Button):
         try:
             send_magic_packet(MAC_ADRESI, ip_address=DIS_IP, port=PORT)
-            await interaction.response.send_message("🚀 Sihirli paket fırlatıldı! PC uyanıyor knk.", ephemeral=True)
+            await interaction.response.send_message("🚀 Sihirli paket fırlatıldı! PC uyanıyor.", ephemeral=True)
         except Exception as e:
             await interaction.response.send_message(f"❌ Hata: {e}", ephemeral=True)
 
 @bot.event
 async def on_ready():
-    print(f'Render Nöbetçi Botu ({bot.user}) Aktif!')
+    print(f'Render Nöbetçi Botu Aktif!')
 
 @bot.command()
 async def panel(ctx):
-    await ctx.send("🎮 **FETTAH REMOTE (NÖBETÇİ)**\nPC kapalıysa 'PC AÇ' butonuna bas knk.", view=ControlPanel())
+    await ctx.send("🎮 **FETTAH REMOTE (RENDER)**", view=ControlPanel())
 
 @bot.command()
 async def ac(ctx):
